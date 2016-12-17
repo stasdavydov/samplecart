7762ccab847c21a694adbe384aca65f6d716a580
Stanislav Davydov <davidovsv@yandex.ru>
2016-12-17 15:28:31 +0700

    Fixed name of dict key to fit the meaning of the app better.

--- backend/server.py
+++ backend/server.py
@@ -6,32 +6,32 @@ from aiohttp import web
 
 
 async def send_updates(request):
-    message = json.dumps(list(request.app['players'].values()))  # list of carts
-    for _ws in request.app['players']:
+    message = json.dumps(list(request.app['customers'].values()))  # list of carts
+    for _ws in request.app['customers']:
         _ws.send_str(message)
 
 
 async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
-    request.app['players'][ws] = []
+    request.app['customers'][ws] = []
     await send_updates(request)
 
     async for msg in ws:
         if msg.tp == aiohttp.MsgType.text:
-            request.app['players'][ws] = json.loads(msg.data)  # cart
+            request.app['customers'][ws] = json.loads(msg.data)  # cart
             print("Somebody shopping")
             await send_updates(request)
         elif msg.tp == aiohttp.MsgType.error:
             print('ws connection closed with exception %s' %
                   ws.exception())
 
-    del request.app['players'][ws]
+    del request.app['customers'][ws]
     return ws
 
 
 loop = asyncio.get_event_loop()
 app = web.Application(loop=loop)
-app['players'] = dict()
+app['customers'] = dict()
 app.router.add_route('GET', '/', websocket_handler)
 web.run_app(app, port=8765)
